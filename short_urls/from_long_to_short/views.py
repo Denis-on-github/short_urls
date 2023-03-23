9e64255906314d98a4b025a6b7eb9f38f50acd53
33306 <3330663@internet.ru>
2023-03-23 16:24:55 +0100

    more beauty

--- short_urls/from_long_to_short/views.py
+++ short_urls/from_long_to_short/views.py
@@ -1,7 +1,8 @@
 from django.contrib import messages
+from django.core.paginator import Paginator
 from django.http import HttpResponseNotFound
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView, FormView
+from django.views.generic import CreateView, FormView, ListView
 from .models import *
 from .forms import *
 
@@ -11,11 +12,12 @@ menu = [{'title': 'Homepage', 'url_name': 'home'},
         {'title': 'Feedback', 'url_name': 'feedback'},
         {'title': 'About me', 'url_name': 'about_me'}]
 
-class HomePage(CreateView):
+class HomePage(CreateView, ListView):
     form_class = NewShortURLForm
     model = ShortURLs
     template_name = 'from_long_to_short/index.html'
-    succes_url = 'feedback'
+    context_object_name = 'all_shorts'
+    paginate_by = 1
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -40,31 +42,6 @@ class HomePage(CreateView):
                                      subpart=subpart,
                                      short_url='http://127.0.0.1:8000/' + subpart + '/')
         return redirect('feedback')
-
-# def index(request):
-#     form = NewShortURLForm(request.POST)
-#     full_url = ''
-#     subpart = ''
-#     short_url = ''
-#     if form.is_valid():
-#         full_url = form.cleaned_data.get('full_url')
-#         subpart = form.cleaned_data.get('subpart')
-#         '''
-#         В данный момент мы сокращаем ссылку и заносим в нашу БД,
-#         БЕЗ проверок на то, есть ли у нас в БД такая ссылка или ее шорт часть!
-#         '''
-#         if len(subpart) != 0:
-#             ShortURLs.objects.create(full_url=full_url,
-#                                      subpart=subpart,
-#                                      short_url='http://127.0.0.1:8000/' + subpart + '/')
-#         else:
-#             subpart = ''.join(random.choices(settings.SYMBOLS, k=settings.LEN_SHORTS))
-#             ShortURLs.objects.create(full_url=full_url,
-#                                      subpart=subpart,
-#                                      short_url='http://127.0.0.1:8000/' + subpart + '/')
-#
-#     context = {'form': form, 'menu': menu, 'title': 'Home page'}
-#     return render(request, 'from_long_to_short/index.html', context=context)
 
 def about_me(request):
     context = {'menu': menu, 'title': 'About me'}
