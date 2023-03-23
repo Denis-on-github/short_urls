879061991498d58235ccf9e640f1ccd5d7c69a35
33306 <3330663@internet.ru>
2023-03-23 12:19:31 +0100

    Smth

--- short_urls/from_long_to_short/urls.py
+++ short_urls/from_long_to_short/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', HomePage.as_view(), name='home'),
+    path('', HomePage.as_view(),  name='home'),
     path('about_me/', about_me, name='about_me'),
     path('feedback/', feedback, name='feedback'),
 ]
