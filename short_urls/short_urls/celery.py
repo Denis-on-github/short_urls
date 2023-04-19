b4c9b95684c6842989ee110b84ee3e65f60b41fd
33306 <3330663@internet.ru>
2023-04-19 15:17:36 +0200

    add Redis add sessions

--- short_urls/short_urls/celery.py
+++ short_urls/short_urls/celery.py
@@ -3,13 +3,11 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-from short_urls import settings
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'short_urls.settings')
 
 app = Celery('short_urls')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
 
 # app.conf.beat_schedule = {
 #     'every': {
