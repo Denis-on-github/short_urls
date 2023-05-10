34810d63fd3964b3bb8464c7247296f24a1b57fe
33306 <3330663@internet.ru>
2023-05-10 14:44:45 +0200

    test

--- short_urls/short_urls/settings.py
+++ short_urls/short_urls/settings.py
@@ -11,8 +11,8 @@ SECRET_KEY = 'django-insecure-h8ub=h8e)qatejt3-sr6c$m*p^irzm2$#9py$57^7+4l!_6)4b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
